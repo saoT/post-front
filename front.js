8524677d7a5499b14f01c9b40bf0e8f08e3ecfda
saraguerric <sara.guerric@evergig.com>
2019-03-01 11:27:54 +0100

    Add : post

--- front.js
+++ front.js
@@ -1,6 +1,7 @@
 console.log($);
 
 // EXEMPLE FAIT AVEC L'API SWAPI Star Wars--------------
+/*
 $.get('https://swapi.co/api/people/', function (response) {
 
   response.results.forEach( function (personnage) {
@@ -10,8 +11,62 @@ $.get('https://swapi.co/api/people/', function (response) {
   });
 
 });
+*/
 //-----------------------------------------------------
+// ON utilise un CLIENT HTTP
+// Une librairie qui permet de requeter des sites
+// via des methodes/verbes HTTP -> GET / POST...
+// Ici on va utiliser jquery, lmais il y en a plein d'autres (axios, $http, superagent, etc...)
 
-$.get('http://localhost:3000/', function (response, error) {
-  console.log(response);
+
+// $.get c'est un peu comme un telephone portable qu'on donne a notre front
+// Par contre quand on compose le numero on est obligé de mettre le 06
+// -> http://localhost:3000
+$.get('http://localhost:3000/students', function (response) {
+  response.forEach(function (student) {
+    const div = document.createElement('div');
+    div.innerHTML = student.student_name;
+    document.body.appendChild(div)
+  })
 });
+
+function send () {
+  var student_name = document.querySelector('#student_name').value;
+  console.log(student_name);
+  $.post('http://localhost:3000/students', {student_name : student_name}, function (response) {
+     console.log(response);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
